b933fed44da8c2a3a672c30c90cbf7bcb8dddd3f
NgawangChoedenShankentsang <ngawangchoeden@icloud.com>
2025-06-08 22:58:03 +0200

    added Entity and Auth

---
+++ src/Entity/Card.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CardRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CardRepository::class)]
+class Card
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $vorname = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nachname = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $linkedinUrl = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $photo = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getVorname(): ?string
+    {
+        return $this->vorname;
+    }
+
+    public function setVorname(string $vorname): static
+    {
+        $this->vorname = $vorname;
+
+        return $this;
+    }
+
+    public function getNachname(): ?string
+    {
+        return $this->nachname;
+    }
+
+    public function setNachname(string $nachname): static
+    {
+        $this->nachname = $nachname;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getLinkedinUrl(): ?string
+    {
+        return $this->linkedinUrl;
+    }
+
+    public function setLinkedinUrl(?string $linkedinUrl): static
+    {
+        $this->linkedinUrl = $linkedinUrl;
+
+        return $this;
+    }
+
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(?string $photo): static
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+}
